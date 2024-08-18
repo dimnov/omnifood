f536cc4d00cfbdc065d7b777a0e43185adfe42b7
dimnov <dimnov1512@gmail.com>
2024-08-18 14:47:03 +0300

    sticky header activation after intersection

--- js/index.js
+++ js/index.js
@@ -13,3 +13,23 @@ allLinks.forEach((link) => {
   });
 });
 
+const sectionHeroEl = document.querySelector(".section-hero");
+const bodyEl = document.body;
+
+const obs = new IntersectionObserver(
+  (entries) => {
+    const ent = entries[0];
+
+    if (!ent.isIntersecting) {
+      bodyEl.classList.add("sticky");
+    } else {
+      bodyEl.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-120px",
+  }
+);
+obs.observe(sectionHeroEl);
